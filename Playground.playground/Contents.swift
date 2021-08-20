652e24268aa1ae7f956b52c58fa540155e596f55
Paula-18 <paunahappy@gmail.com>
2021-08-20 15:58:13 -0700

    20-agosto

---
+++ Playground.playground/Contents.swift
@@ -0,0 +1,45 @@
+import Foundation
+
+//print("Hello World")
+//print("Hello 2")
+
+var nombre = "Natalia"
+nombre = "Paula"
+
+let historia = "Había una vez ..."
+//historia = "Erase una vez ..."
+
+/*
+varias
+líneas de 
+comentario
+*/
+
+//print(nombre)
+//print(historia)
+
+var mensajeBienvenida : String
+mensajeBienvenida = "Hola \(nombre)"
+//print(mensajeBienvenida)
+
+var 原神 = "ベンティ"
+//print(原神)
+
+var entero : Int = 5
+var flotante : Float = 1.8
+var doble : Double = 464374823526434636727.43784637
+//print(doble)
+
+var booleano = true
+
+if booleano {
+    print("Se cumplio la condicion")
+}
+
+var a = 4
+var b = 5
+if (b>a){
+    print("B es mayor que A")
+}else{
+    print("B es menor que A")
+}
